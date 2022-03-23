ffe7e915e1b9bbcda13973725c70c1c95c36a4c4
mgrrtt <magritm@yandex.com>
2022-03-23 12:24:38 +0300

    second step

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ lint:
 	npx stylelint ./src/styles/*.css
 	npx stylelint ./src/styles/**/*.scss
 	npx htmlhint ./src/*.html
+
+deploy:
+	npx surge ./src/
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const { src, dest, parallel, watch } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+const browserSyncJob = () => {
+  browserSync.init({
+    server: 'src/'
+  });
+
+  watch('src/styles/**/*.scss', { event: 'all' }, compileSass);
+  watch('src/*.html', { event: 'all' }, updateHtml);
+};
+
+const updateHtml = () => {
+  console.log('update my html...');
+
+  return src('src/index.html')
+    .pipe(browserSync.stream());
+};
+
+const compileSass = () => {
+  console.log('compile my sass to css...');
+
+  return src('src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(dest('src/styles'))
+    .pipe(browserSync.stream());
+};
+
+const buildHtml = () => {
+  console.log('copy my html to build...');
+
+  return src('src/index.html')
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
+};
+
+const buildFonts = () => {
+  console.log('copy my fonts to build...');
+
+  return src('src/fonts/*')
+    .pipe(dest('build/fonts/'));
+};
+
+const buildImages = () => {
+  console.log('copy my images to build...');
+
+  return src('src/images/*')
+    .pipe(dest('build/images/'));
+};
+
+const buildStyles = () => {
+  console.log('build project...');
+
+  return src('src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(concat('style.css'))
+    .pipe(dest('build/styles'));
+};
+
+exports.default = compileSass;
+exports.watchers = browserSyncJob;
+exports.build = parallel(buildHtml, buildFonts, buildImages, buildStyles);
